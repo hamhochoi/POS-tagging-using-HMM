78a1d3612280e6d124a0e1e1c5f6ff2365696634
hamhochoi <nichphu@gmail.com>
2017-10-26 13:14:50 +0700

    Update pos_tagging.py

--- pos_tagging.py
+++ pos_tagging.py
@@ -2,9 +2,12 @@ import os
 from read_next_token import read_next_token
 from find_word_in_dictionary import find_word_label
 
-untagged_path = "D:\\OneDrive for Business 1\\OneDrive - student.hust.edu.vn\\OD\\20171\\NLP\\Project\\pos_tagging_materials\\datasets\\untagged"
-path = "D:\\OneDrive for Business 1\\OneDrive - student.hust.edu.vn\\OD\\20171\\NLP\\Project\\pos_tagging_materials\\datasets\\brown"
+dict_path = "D:\\OneDrive for Business 1\\OneDrive - student.hust.edu.vn\\OD\\20171\\NLP\\Project\\pos_tagging_materials\\datasets\\brown\\dictionary.txt"
+f_dict = open(dict_path, 'r')
+f_read_dict = f_dict.read()
+f_read_dict = f_read_dict.split('\n')
 
+untagged_path = "D:\\OneDrive for Business 1\\OneDrive - student.hust.edu.vn\\OD\\20171\\NLP\\Project\\pos_tagging_materials\\datasets\\untagged"
 untagged_dir = os.listdir(untagged_path)
 untagged_len = len(untagged_dir)
 
@@ -23,12 +26,14 @@ for i in range(untagged_len):
 	current_word = 0;	# To append 2 fake word in beginning of line.
 	word = ''
 	
-	while (word != "EOF"):		
+	while (word != "EOF"):
+		# read 3 word
 		word = read_next_token(f_read, current_line, current_word)
 		pre_word = read_next_token(f_read, current_line, current_word-1)
 		pre_pre_word = read_next_token(f_read, current_line, current_word-2)
 		
 		current_word = current_word + 1
+		
 		if (word == "EOL"):
 			current_line = current_line + 1;
 			current_word = 0
@@ -36,9 +41,10 @@ for i in range(untagged_len):
 			continue;
 		print (word)
 		
-		labels = find_word_label(f_read, word)
-		pre_labels = find_word_label(f_read, pre_word)
-		pre_pre_labels = find_word_label(f_read, pre_pre_word)
+		# read 3 word's labels
+		labels = find_word_label(f_read_dict, word)
+		pre_labels = find_word_label(f_read_dict, pre_word)
+		pre_pre_labels = find_word_label(f_read_dict, pre_pre_word)
 		
 		if (labels == -1):
 			labels = all_labels
